2eb3a23a1692e45758eab1eca0693c6cd52b24ff
Nahida <1139500183@qq.com>
2024-02-17 17:40:53 +0800

    add

---
+++ start(虚拟驱动器版).bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+subst I: virtual
+virtual\SoftWare\anaconda3\envs\dataset\Scripts\streamlit.exe  run dataset.py --server.port 2323
---
+++ start.bat
@@ -0,0 +1,2 @@
+cd /d %~dp0
+streamlit run dataset.py --server.port 2323
